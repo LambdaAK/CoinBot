c3131f4a8c463b61a0465406d2d217350c6ab906
Alex Kozik <alex.kozik@yahoo.com>
2025-07-26 19:57:26 -0700

    Fixed a bug related to matrix multiplication dimensions.

--- src/DQN_agent.py
+++ src/DQN_agent.py
@@ -18,16 +18,20 @@ from typing import List, Tuple, Dict, Any
 import time
 
 class CoinCollectionDQN(nn.Module):
-    """Simpler neural network for coin collection DQN agent"""
+    """Neural network for coin collection DQN agent"""
     
     def __init__(self, state_size: int, action_size: int):
         super(CoinCollectionDQN, self).__init__()
-        self.fc1 = nn.Linear(state_size, 64)
-        self.fc2 = nn.Linear(64, action_size)
+        self.fc1 = nn.Linear(state_size, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 128)
+        self.fc4 = nn.Linear(128, action_size)
         
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
         return x
 
 class CoinCollectionAgent:
